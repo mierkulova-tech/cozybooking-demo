ccf4a348ec7babe59b85c91e0b6768ab79fc0ad2
mierkulova-tech <mierkulova.tech@gmail.com>
2026-07-21 01:07:03 +0200

    fix: use get_wsgi_application instead of get_asgi_application in wsgi.py
    wsgi.py was incorrectly calling the ASGI entrypoint, causing
    TypeError: ASGIHandler.__call__() missing 1 required positional
    argument: 'send' under both runserver and gunicorn (affects prod).

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+application = get_wsgi_application()
